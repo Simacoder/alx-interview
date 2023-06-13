bccb1b30f343baf8f939a045bfcd5078017a721b
Simanga <simacoder@hotmail.com>
2023-06-13 18:14:35 +0000

    task change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,20 +6,13 @@
 
 def makeChange(coins, total):
     """
-	Given a pile of coins of different values, determine the fewest number 
-	of coins needed to meet a given amount total.
+       Given a pile of coins of different values determine,
+       the fewest number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
-
-    # Create an array to store the minimum number of coins required to reach each value
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
-            # Update the minimum number of coins required for each value
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-
-    # Return the minimum number of coins required to reach the total value
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
